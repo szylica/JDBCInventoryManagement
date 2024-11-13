d42de92afb232fdaeef0a2d19137f6debd363af8
szylica <szymonsta12@onet.pl>
2024-11-13 19:35:53 +0100

    create JDBC connection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.sql.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String sql = "select * from employees where officeCode=3";
+        String url = "jdbc:mysql://localhost:3306/classicmodels";
+        String user = "root";
+        String password = "szPYa&bqH5lFShl";
+
+        try(Connection connection = DriverManager.getConnection(url, user, password)){
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("lastname"));
+                System.out.println(resultSet.getString("firstname"));
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
